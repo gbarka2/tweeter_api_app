007e6b91e9b7ad4f113f4f2debf2280e0bf1cafe
Gary Barkauski <gbarka2@gmail.com>
2021-02-23 19:34:05 -0700

    Commit 5: Added routes and controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,23 @@
+class TweetsController < ApplicationController
+
+  before_action :get_tweet, only: [:show, :update, :destroy]
+
+  def index
+    render json: {status: 200, tweets: Tweet.all}
+  end
+
+  def show
+    render json: {status: 200, tweet: @tweet}
+  end
+
+  private
+
+  def tweet_params
+    params.required(:tweet).permit(:title, :content, :author)
+  end
+
+  def get_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+  
+end
